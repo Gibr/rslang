20835db6b07a81238df7cf1952e73b736444b9e1
PanchenkoAleksandr <aleksandr.panchenko@tut.by>
2022-01-24 21:30:16 +0300

    feat: implement router and and navigation between 4 pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import Router from './components/router/Router';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
