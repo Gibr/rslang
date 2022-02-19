9b91a4627bbcaf3d86019bdaa41e8f22d396d846
Denis-Politsarnov <todayz@yandex.ru>
2022-02-19 15:37:00 +0300

    feat:  implement login/registration connection to server

--- src/components/Forms/RegForm.tsx
+++ src/components/Forms/RegForm.tsx
@@ -1,16 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Form.scss';
 import { NavLink } from 'react-router-dom';
 import AppRoutes from '../../app/constants/routes';
 import { switchPopup } from '../LoginBtn/LoginBtnSlice';
 import Input from '../Input/Input';
 import { useAppDispatch } from '../../app/hooks';
+import createUser from '../../api/auth/registration';
+import signIn from '../../api/auth/auth';
 
 function RegForm(): JSX.Element {
   const dispatch = useAppDispatch();
+  const [error, setError] = useState({ isVisible: false, text: '' });
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    setError({
+      isVisible: false,
+      text: '',
+    });
+
+    const targets = event.target as HTMLFormElement;
+
+    if (
+      (targets[2] as HTMLInputElement).value !==
+      (targets[3] as HTMLInputElement).value
+    ) {
+      setError({
+        isVisible: true,
+        text: 'You entered two different passwords. Please try again',
+      });
+    } else {
+      const name = (targets[0] as HTMLInputElement).value;
+      const email = (targets[1] as HTMLInputElement).value;
+      const password = (targets[2] as HTMLInputElement).value;
+
+      createUser({ name, email, password })
+        .then(() => {
+          signIn({ email, password }).catch((err) => {
+            setError({
+              isVisible: true,
+              text: err.message,
+            });
+          });
+        })
+        .catch((err) => {
+          setError({
+            isVisible: true,
+            text: err.message,
+          });
+        });
+    }
+  };
 
   return (
-    <form action="" className="form">
+    <form onSubmit={handleSubmit} className="form">
       <div className="form-row">
         <div className="form-title">Registration</div>
       </div>
@@ -19,6 +61,12 @@ function RegForm(): JSX.Element {
       <Input type="password" text="Password:" />
       <Input type="password" text="Confirm password:" />
       <Input type="submit" text="Confirm" />
+      {error.isVisible && (
+        <div className="form-row form-row_disabled">
+          <div className="form-text form-text-error">{error.text}</div>
+        </div>
+      )}
+
       <div className="form-row">
         <div className="form-text">Do you already have an account?</div>
         <NavLink
